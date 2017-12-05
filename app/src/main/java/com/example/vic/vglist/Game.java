f72ecbcbffe4d056f096ea91865a4f63e52f67ba
vila6 <mrvila@esei.uvigo.es>
2017-12-05 16:57:13 +0100

    Initial commit

---
+++ app/src/main/java/com/example/vic/vglist/Game.java
@@ -0,0 +1,11 @@
+package com.example.vic.vglist;
+
+/**
+ * Created by Mau on 05/12/2017.
+ */
+
+public class Game {
+    public Game(){
+        
+    }
+}
---
+++ app/src/main/java/com/example/vic/vglist/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.vic.vglist;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.android.volley.VolleyError;
+import com.igdb.api_android_java.callback.onSuccessCallback;
+import com.igdb.api_android_java.model.APIWrapper;
+import com.igdb.api_android_java.model.Parameters;
+
+import org.json.*;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String API_KEY = "cf62b2e3faec9509618f5ddc1125e937";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final TextView resultado = (TextView) findViewById(R.id.resultado);
+        final EditText busqueda = (EditText) findViewById(R.id.busqueda);
+
+        busqueda.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                APIWrapper wrapper = new APIWrapper(getApplicationContext(), API_KEY);
+                Parameters params = new Parameters()
+                        .addSearch(busqueda.getText().toString())
+                        .addFields("name,cover.url")
+                        .addOrder("published_at:desc")
+                        .addLimit("20");
+
+                wrapper.games(params, new onSuccessCallback(){
+                    @Override
+                    public void onSuccess(JSONArray result) {
+                        // Do something with resulting JSONArray
+                        ArrayList<String> resultArray = null;
+                        try {
+                            resultArray = JSONtoString(result);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                        if(!resultArray.isEmpty()) {
+                            resultado.setText("Resultados: " + result.length() + "\n" + resultArray.toString());
+                        }else{
+                            resultado.setText("Busca bien carallo");
+                        }
+                    }
+
+                    @Override
+                    public void onError(VolleyError error) {
+                        System.err.print("Fallito wapo");
+                    }
+                });
+            }
+        });
+
+
+    }
+    public static ArrayList<String> JSONtoString(JSONArray jsonArray) throws JSONException {
+        ArrayList<String> list = new ArrayList<String>();
+        if (jsonArray != null) {
+            jsonArray.getString(1);
+            int len = jsonArray.length();
+            for (int i = 0; i < len; i++) {
+                list.add(jsonArray.get(i).toString());
+            }
+        }
+        return list;
+    }
+}
