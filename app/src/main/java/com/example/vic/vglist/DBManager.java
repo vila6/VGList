a3932d05155832e06dd509de16672880b81aa2a3
Mauro Rodríguez Vila <mrvila@esei.uvigo.es>
2017-12-15 07:02:04 +0100

    Interfaz mejorada con estrellitas

--- app/src/main/java/com/example/vic/vglist/DBManager.java
+++ app/src/main/java/com/example/vic/vglist/DBManager.java
@@ -1,9 +1,12 @@
 package com.example.vic.vglist;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.sql.SQLException;
 
@@ -14,17 +17,14 @@ import java.sql.SQLException;
 public class DBManager extends SQLiteOpenHelper {
 
     public DBManager(Context context) {
-        super( context, "DB_VGList", null,1);
+        super( context, "VGList_DB", null,1);
     }
     @Override
     public void onCreate(SQLiteDatabase db)
     {
         try {
             db.beginTransaction();
-            db.execSQL( "CREATE TABLE IF NOT EXISTS 'JUEGOS' ("
-                    + "ID" + " int PRIMARY KEY,"
-                    + "NOMBRE" + " string(255) NOT NULL"
-                    + ")" );
+            db.execSQL( "CREATE TABLE JUEGOS ( ID int PRIMARY KEY, NAME TEXT, PERSONALRATING int)");
             db.setTransactionSuccessful();
         } finally {
             db.endTransaction();
@@ -33,8 +33,39 @@ public class DBManager extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS JUEGOS");
+        onCreate(sqLiteDatabase);
     }
-    // más cosas…
+
+    public void addGame(int id, String name, int personalRating){
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("ID",id);
+        contentValues.put("NAME",name);
+        contentValues.put("PERSONALRATING",personalRating);
+        this.getWritableDatabase().insertOrThrow("JUEGOS",null,contentValues);
+    }
+
+    public void deleteGame(int id){
+        this.getWritableDatabase().delete("JUEGOS","ID='"+id+"'",null);
+    }
+
+    public void updateGame(int id, int new_rating){
+        this.getWritableDatabase().execSQL("UPDATE JUEGOS SET PERSONALRATING='" + new_rating + "' WHERE ID='" + id + "'");
+    }
+
+    public void list_all_games(TextView textView){
+        Cursor cursor = this.getReadableDatabase().rawQuery("SELECT * FROM JUEGOS",null);
+        while(cursor.moveToNext()){
+            textView.append(cursor.getString(1) + " " + cursor.getString(2));
+        }
+    }
+
+    public void echoAll(){
+        Cursor cursor = this.getReadableDatabase().rawQuery("SELECT * FROM JUEGOS", null);
+        while(cursor.moveToNext()){
+            System.out.println(cursor.getString(1) + " " + cursor.getString(2));
+        }
+    }
+
 }
 
--- app/src/main/java/com/example/vic/vglist/SearchActivity.java
+++ app/src/main/java/com/example/vic/vglist/SearchActivity.java
@@ -5,10 +5,24 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.SearchView;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.android.volley.VolleyError;
+import com.bumptech.glide.Glide;
 import com.igdb.api_android_java.callback.onSuccessCallback;
 import com.igdb.api_android_java.model.APIWrapper;
 import com.igdb.api_android_java.model.Parameters;
@@ -18,61 +32,59 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class SearchActivity extends AppCompatActivity {
-
+    public List<Game> gamesList = new ArrayList<Game>();
     private final String API_KEY = "cf62b2e3faec9509618f5ddc1125e937";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public boolean onCreateOptionsMenu(Menu menu) {
+        final DBManager dbManager = new DBManager(getApplicationContext());
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
-  
-        final TextView resultado = (TextView) findViewById(R.id.resultado);
-        final EditText busqueda = (EditText) findViewById(R.id.busqueda);
+        super.onCreateOptionsMenu(menu);
 
-        busqueda.addTextChangedListener(new TextWatcher() {
+        this.getMenuInflater().inflate(R.menu.main_menu, menu);
+        MenuItem searcher = menu.findItem(R.id.btSearch);
+        SearchView searchView = (SearchView) searcher.getActionView();
+
+        ListView listGames = (ListView) findViewById(R.id.listGames);
+        final CustomAdapter adapter = new CustomAdapter();
+        listGames.setAdapter(adapter);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                resultado.setText("Buscando...");
+            public boolean onQueryTextSubmit(String busqueda) {
+                System.out.println("BUSCANDO: "+busqueda);
                 APIWrapper wrapper = new APIWrapper(getApplicationContext(), API_KEY);
                 Parameters params = new Parameters()
-                        .addSearch(busqueda.getText().toString())
+                        .addSearch(busqueda)
                         .addFields("name,rating,cover")
                         .addOrder("popularity:desc")
                         .addLimit("20");
 
-                wrapper.games(params, new onSuccessCallback(){
+                wrapper.games(params, new onSuccessCallback() {
                     @Override
-                    public void onSuccess(JSONArray result)  {
+                    public void onSuccess(JSONArray result) {
+                        gamesList.clear();
                         for (int i = 0; i < result.length(); i++) {
                             try {
                                 JSONObject jsonobject = result.getJSONObject(i);
                                 int id = jsonobject.getInt("id");
                                 String name = jsonobject.getString("name");
-                                String url="";
-                                if(jsonobject.has("cover")){
+                                String url = "";
+                                if (jsonobject.has("cover")) {
                                     JSONObject cover = jsonobject.getJSONObject("cover");
                                     url = cover.getString("url");
                                 }
-                                float rating=0;
-                                if(jsonobject.has("rating")){
+                                float rating = 0;
+                                if (jsonobject.has("rating")) {
                                     rating = (float) jsonobject.getDouble("rating");
                                 }
                                 Game juego = new Game(id, name, rating, url);
-                                System.out.println(juego.toString());
-                                resultado.setText(resultado.getText() + juego.toString());
+                                gamesList.add(juego);
+                                dbManager.echoAll();
+                                adapter.notifyDataSetChanged();
+
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -85,11 +97,104 @@ public class SearchActivity extends AppCompatActivity {
                         System.err.print("Fallito wapo");
                     }
                 });
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                return false;
+            }
+        });
+        return true;
+        }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        APIWrapper wrapper = new APIWrapper(getApplicationContext(), API_KEY);
+        Parameters params = new Parameters()
+                .addOrder("popularity:desc")
+                .addLimit("20");
+
+        wrapper.games(params, new onSuccessCallback() {
+            @Override
+            public void onSuccess(JSONArray result) {
+                gamesList.clear();
+                for (int i = 0; i < result.length(); i++) {
+                    try {
+                        JSONObject jsonobject = result.getJSONObject(i);
+                        int id = jsonobject.getInt("id");
+                        String name = jsonobject.getString("name");
+                        String url = "";
+                        if (jsonobject.has("cover")) {
+                            JSONObject cover = jsonobject.getJSONObject("cover");
+                            url = cover.getString("url");
+                        }
+                        float rating = 0;
+                        if (jsonobject.has("rating")) {
+                            rating = (float) jsonobject.getDouble("rating");
+                        }
+                        Game juego = new Game(id, name, rating, url);
+                        gamesList.add(juego);
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+
+            @Override
+            public void onError(VolleyError error) {
+                System.err.print("Fallito wapo");
             }
         });
 
 
     }
+
+    class CustomAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return 20;
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return gamesList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            view = getLayoutInflater().inflate(R.layout.list_item,null);
+
+            ImageView imageView = (ImageView)view.findViewById(R.id.imgCover);
+            TextView textGame = (TextView) view.findViewById(R.id.textGameName);
+            RatingBar ratingBar = (RatingBar) view.findViewById(R.id.ratingBar);
+
+            if(gamesList.size()>i) {
+                Game actualGame = gamesList.get(i);
+                Glide.with(getApplicationContext())
+                        .load(actualGame.getCoverUrl())
+                        .into(imageView);
+                textGame.setText((CharSequence) actualGame.getName());
+                ratingBar.setRating(actualGame.getRating()/10);
+                System.out.println("Juego numero " + i + ": " + actualGame.getName());
+            }
+
+
+            return view;
+        }
+    }
+
     public static ArrayList<String> JSONtoString(JSONArray jsonArray) throws JSONException {
         ArrayList<String> list = new ArrayList<String>();
         if (jsonArray != null) {
